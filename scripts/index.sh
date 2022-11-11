192ad14986ba09276350ef4e256e724a1b12f1d1
Miguel Ramon <nozerorma@gmail.com>
2022-11-11 18:37:18 +0100

    Functional index script

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,8 +1,10 @@
+#### INDEX SCRIPT ####
+
 # This script should index the genome file specified in the first argument ($1),
 # creating the index in a directory specified by the second argument ($2).
 
-# The STAR command is provided for you. You should replace the parts surrounded
-# by "<>" and uncomment it.
+contaminants=$1
+outdir=$2
 
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
-# --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+STAR 	--runThreadN 6 --runMode genomeGenerate --genomeDir ${outdir} \
+	--genomeFastaFiles ${contaminants} --genomeSAindexNbases 9
