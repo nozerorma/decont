6b85b24909804c379419557119ffc87542ec199d
Miguel Ramon <nozerorma@gmail.com>
2022-11-11 22:43:53 +0100

    Removed comments

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -4,11 +4,3 @@ sampledir=$1
 outdir=$2
 sampleid=$3
 cat $sampledir/${sampleid}-* >> ${outdir}/${sampleid}
-
-
-
-# This script should merge all files from a given sample (the sample id is
-# provided in the third argument ($3)) into a single file, which should be
-# stored in the output directory specified by the second argument ($2).
-#
-# The directory containing the samples is indicated by the first argument ($1).
