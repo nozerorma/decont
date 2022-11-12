b38ddcf714bb2ec221b26ebab331fd8fcc34a410
Miguel Ramon <nozerorma@gmail.com>
2022-11-12 21:57:17 +0100

    Working cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,22 +3,27 @@
 if [ $# -eq 1 ]
 then
         debris=$1
-        if [ $debris == "Y" ]
+        if [[ "${debris}" = *[Yy]* ]]
         then
                 echo -e "\nCleaning everything up...\n"
-                find data/* log/* out/* res/* ! \( -name 'urls' -o -name '.gitkeep' -o -name '*.log' \) -exec rm -f {} \; # Exclude gitkeeps, urls, scripts and logs 
+                find data/* log/* out/* res/* -mindepth 1! \( -name 'urls' -o -name '.gitkeep' -o -name '*.log' \) -exec rm -rf {} \; 
+		# Exclude gitkeeps, urls, scripts and logs 
 	fi
 else
         echo -e "Would you like to remove any remaining files from previous runs? Y/n\n"
         read debris
-        if [ $debris == "Y" ]
+        echo
+	if [[ ${debris} = *[Yy]* ]]
         then
                 echo -e "Which files would you like to remove? data log out res\n"
-                read debris
-                if [ $debris == "data" ] || [ $debris == "log" ] || [ $debris == "out" ] || [$debris == "res" ]
-		then	
-			echo -e "Cleaning up $debris\n"
-			find $debris/* ! \( -name 'urls' -o -name '.gitkeep' \) -exec rm -f {} \; 2> log/cleanup_err.log # Cleanse selectively
+                read cleandebris
+                expectedinput="data log out res"
+		echo
+		if [[ "$expectedinput" == *"$cleandebris"* ]]
+		then
+			echo -e "Cleaning up $cleandebris\n"
+                        find $cleandebris/* -mindepth 1 ! \( -name 'urls' -o -name '.gitkeep' \) -exec rm -rf {} \; 2> log/cleanup_err.log 
+                        # Cleanse selectively
         	else
 			echo -e "No cleanup performed\n"
 		fi
