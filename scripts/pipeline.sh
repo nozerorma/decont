0892e8d6a3e726122181c24af1f8ed631fadd1d0
Miguel Ramon <nozerorma@gmail.com>
2022-11-15 11:33:40 +0100

    Optimized global log and steps comments

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -4,19 +4,29 @@ echo ####### RNA DECONTAMINATION PIPELINE by Miguel Ramón Alonso #######
 bash scripts/cleanup.sh 2>> log/errors.log
 
 set -e
+
 echo -e "Downloading required files...\n"
 mkdir -p data
+
+
 # Download and extract required genomes
+
 for url in $(grep 'https' data/urls | grep -v 'contaminants' | sort -u)
 do
         bash scripts/download.sh $url data yes 2>> log/errors.log
 done
 
+
 # Download, extract and filter decontaminants database
+
 url=$(grep 'contaminants' data/urls)
 bash scripts/download.sh $url res yes filt 2>> log/errors.log
 
+
+# STAR Index building
+
 echo -e "\nBuilding contaminants database index...\n"
+
 if [ ! "$(ls -A "res/contaminants_idx" 2>> log/errors.log)" ]
 then
         # Build contaminants index
@@ -26,6 +36,7 @@ else
 fi
 
 mkdir -p out && mkdir -p out/merged
+
 for sid in $(find data -name *.fastq -exec basename {} \; | cut -d"-" -f1 | sort -u)
 do
         echo -e "Merging $sid sample files together...\n"
@@ -33,7 +44,11 @@ do
         bash scripts/merge_fastqs.sh data out/merged $sid
 done
 
+
+# Cutadapt trimming step
+
 echo -e "\nRemoving adapters...\n"
+
 if [ ! "$(ls -A "out/trimmed" 2>> log/errors.log)" ] 
 then
         mkdir -p out/trimmed && trimDir="out/trimmed"
@@ -52,7 +67,11 @@ else
         echo -e "Adapters already trimmed, skipping trimming\n" 
 fi
 
+
+# STAR alignment step
+
 echo -e "\nAligning reads to contaminants. Outputing non-aligned reads...\n"
+
 if [ ! "$(ls -A "out/star" 2>> log/errors.log)" ] 
 then
         mkdir -p out/star/$basenameSid && starDir="out/star"
@@ -69,27 +88,27 @@ else
         echo -e "Alignament already performed, skipping alingment\n"
 fi
 
+
+# Saving common log
+
 echo -e "\nSaving a common log with information on trimming and alignment results...\n"
 
-# I don't like this approach, search alt
 echo -e "\nGlobal log input as of $(date +'%x                %H:%M:%S')\n" >> pipeline.log
 echo >> pipeline.log
+
 for basenameSid in $(find out/trimmed -name \* -type f -exec basename {} .fastq.gz \; | cut -d"_" -f-2)
 do
         echo -e "$basenameSid STAR analysis\n" >> pipeline.log
-        grep 'Uniquely mapped reads %' out/star/$basenameSid/Log.final.out | \
-		awk -v OFS=' ' '{$1=$1}1' >> pipeline.log
-        grep '% of reads mapped to too many loci' out/star/$basenameSid/Log.final.out | \
-		awk -v OFS=' ' '{$1=$1}1' >> pipeline.log
-        grep '% of reads mapped to multiple loci' out/star/$basenameSid/Log.final.out | \
+        grep -E 'Uniquely mapped reads % | % of reads mapped to too many loci | % of reads mapped to multiple loci' \
+		out/star/$basenameSid/Log.final.out | \
 		awk -v OFS=' ' '{$1=$1}1' >> pipeline.log
 	echo >> pipeline.log
+	
 	echo -e "$basenameSid cutadapt analysis\n" >> pipeline.log
-        grep 'Reads with adapters' log/cutadapt/$basenameSid.log | \
-		awk -v OFS=' ' '{$1=$1}1' >> pipeline.log
-        grep 'Total basepairs' log/cutadapt/$basenameSid.log | \
+        grep 'Reads with adapters | Total basepairs' log/cutadapt/$basenameSid.log | \
 		awk -v OFS=' ' '{$1=$1}1' >> pipeline.log
         echo >> pipeline.log
 done
+
 
 echo -e "\n############ Pipeline finished at $(date +'%H:%M:%S') ##############\n"
