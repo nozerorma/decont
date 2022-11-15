8b3b50e862d0532793724685a8896cb7b9e620d5
Miguel Ramon <nozerorma@gmail.com>
2022-11-15 12:14:39 +0100

    polished cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,30 +1,39 @@
 #### CLEANUP SCRIPT ####
 
-if [ $# -eq 1 ]
+if [ $# -eq 1 ] 
 then
-        debris=$1
-        if [[ "${debris}" = *[Yy]* ]]
+        # Works mainly as rollback script when certain situations do not comply
+	
+	debris=$1
+        
+	if [[ "${debris}" = *[Yy]* ]]
         then
                 echo -e "\nCleaning everything up...\n"
                 find data log out res -mindepth 1 ! \( -name 'urls' -o -name '.gitkeep' -o -name '*.log' \) -exec rm -rf {} \; 
 		# Exclude gitkeeps, urls, scripts and logs 
 	fi
+
+# Works both as standalone cleanup script and interactive startup cleanup script
+
 else
         echo -e "Would you like to remove any remaining files from previous runs? Y/n\n"
         read debris
         echo
+	
 	if [[ ${debris} = *[Yy]* ]]
         then
-                echo -e "Which files would you like to remove? data log out res\n"
+                # Cleanup selectively
+	
+		echo -e "Which files would you like to remove? data log out res\n"
                 read cleandebris
                 expectedinput="data log out res"
 		echo
+		
 		if [[ "$expectedinput" == *"$cleandebris"* ]]
 		then
 			echo -e "Cleaning up $cleandebris\n"
                         find $cleandebris -mindepth 1 ! \( -name 'urls' -o -name '.gitkeep' \) -exec rm -rf {} \; 2> log/cleanup_err.log 
-                        # Cleanse selectively
-        	else
+               	else
 			echo -e "No cleanup performed\n"
 		fi
 	else
