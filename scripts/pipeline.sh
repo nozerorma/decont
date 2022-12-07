fd4fd723504bfe73ce63f23c4c0d51c5cf49c7e6
Miguel Ramón <nozerorma@gmail.com>
2022-12-07 17:41:47 +0100

    Updated common log command for other column ver

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -98,16 +98,28 @@ echo -e "___________________________________________________________\n">> pipeli
 
 for basenameSid in $(find out/trimmed -name \* -type f -exec basename {} .fastq.gz \; | cut -d"_" -f-2)
 do
-        echo -e "$basenameSid STAR analysis\n" | sed $'s/^/\t /' >> pipeline.log
-        grep -E 'Uniquely mapped reads %|% of reads mapped to too many loci|% of reads mapped to multiple loci' \
-		out/star/$basenameSid/Log.final.out | \
-		awk -v OFS=' ' '{$1=$1}1' | sed $'s/^/\t\t- /;s/ |/:/g' | column -t -s: -o$'\t\t' >> pipeline.log
-	echo >> pipeline.log
-	
-	echo -e "$basenameSid cutadapt analysis\n" | sed $'s/^/\t/' >> pipeline.log
-        grep -E 'Reads with adapters|Total basepairs' log/cutadapt/$basenameSid.log | \
-		awk -v OFS=' ' '{$1=$1}1' | sed $'s/^/\t\t- /' | column -t -s: -o$'\t\t\t' >> pipeline.log
-        echo -e "\n\n" >> pipeline.log
+	if [[ $(dpkg -S $(which column) | grep bsdmain) == *[bsdmainutils]* ]]
+	then	
+	        echo -e "$basenameSid STAR analysis\n" | sed $'s/^/\t /' >> pipeline.log
+		grep -E 'Uniquely mapped reads %|% of reads mapped to too many loci|% of reads mapped to multiple loci' \
+			out/star/$basenameSid/Log.final.out | \
+			awk -v OFS=' ' '{$1=$1}1' | sed $'s/^/\t\t- /;s/ |/:/g' | column -t -s:  >> pipeline.log
+		echo >> pipeline.log
+		echo -e "$basenameSid cutadapt analysis\n" | sed $'s/^/\t/' >> pipeline.log
+        	grep -E 'Reads with adapters|Total basepairs' log/cutadapt/$basenameSid.log | \
+		awk -v OFS=' ' '{$1=$1}1' | sed $'s/^/\t\t- /' | column -t -s:  >> pipeline.log
+		echo -e "\n\n" >> pipeline.log
+	else
+		echo -e "$basenameSid STAR analysis\n" | sed $'s/^/\t /' >> pipeline.log
+                grep -E 'Uniquely mapped reads %|% of reads mapped to too many loci|% of reads mapped to multiple loci' \
+                        out/star/$basenameSid/Log.final.out | \
+                        awk -v OFS=' ' '{$1=$1}1' | sed $'s/^/\t\t- /;s/ |/:/g' | column -t -s: -o$'\t\t' >> pipeline.log
+                echo >> pipeline.log
+                echo -e "$basenameSid cutadapt analysis\n" | sed $'s/^/\t/' >> pipeline.log
+                grep -E 'Reads with adapters|Total basepairs' log/cutadapt/$basenameSid.log | \
+                awk -v OFS=' ' '{$1=$1}1' | sed $'s/^/\t\t- /' | column -t -s: -o$'\t\t\t' >> pipeline.log
+                echo -e "\n\n" >> pipeline.log
+	fi
 done
 
 echo -e "\nCommon log saved in /pipeline.log\n" 
