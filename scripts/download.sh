25e6fecb6967370615d5f6e194b14af62517b403
Miguel Ramon <nozerorma@gmail.com>
2022-12-13 12:52:09 +0100

    Working filtering logic

--- scripts/download.sh
+++ scripts/download.sh
@@ -33,7 +33,8 @@ then
 		echo -e "\nRemoving small nuclear sequences from contaminants database...\n"
 		sampleid=$(basename $downloadurl .gz)
 		mv $directoryurl/$sampleid $directoryurl/unfiltered_$sampleid
-		grep -vwE "small nuclear" $directoryurl/unfiltered_$sampleid > $directoryurl/$sampleid	
+		awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' $directoryurl/unfiltered_$sampleid | \
+		grep -vw 'small nuclear' $directoryurl/unfiltered_$sampleid > $directoryurl/$sampleid	
 	fi
 
 else
