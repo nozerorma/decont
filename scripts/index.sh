3e7b64b60bb07c01c53fd13912172947a4069e98
Miguel Ramon <nozerorma@gmail.com>
2022-12-14 12:21:24 +0100

    removed unnecessary brackets

--- scripts/index.sh
+++ scripts/index.sh
@@ -5,6 +5,6 @@ outdir=$2
 
 echo -e "\n### Default number of threads set to 6, please modify if neccessary ###\n"
 
-STAR 	--runThreadN 6 --runMode genomeGenerate --genomeDir ${outdir} \
-	--genomeFastaFiles ${contaminants} --genomeSAindexNbases 9
+STAR 	--runThreadN 6 --runMode genomeGenerate --genomeDir $outdir \
+	--genomeFastaFiles $contaminants --genomeSAindexNbases 9
 echo
