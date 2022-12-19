5ee6213c37ea82732a27c8d5eac325cad2d03cb3
Miguel Ramón <nozerorma@gmail.com>
2022-12-19 14:09:28 +0100

    Swapped awk/grep approach for seqkit/grep approach. Added verbosity.
    Added an explanation on way awk approach may not be the best. It could work if the awk program could reverse the line breakage afterwards,
    returning the FASTA file to its normal structure. When alignment was performed using both approaches, different base scores were obtained.
    This clarifies that, although decontaminants.fastq file is indeed filtered, the deformation of its structure unallows STAR to recognize the
    file, thus not performing the alignment accordingly.
    Rest is just verbosity for clarification of the performed tasks

--- scripts/download.sh
+++ scripts/download.sh
@@ -19,13 +19,19 @@ then
 	else
 		echo -e "Download integrity could not be verified. Aborting...\n"
 		cd ..
+		
 		# Rollback function commented out as it may not be recommendable. See cleanup.sh.
 		# bash scripts/cleanup.sh Y
+
 		echo -e "\n############ Pipeline failed at $(date +'%H:%M:%S') ##############\n"
 		exit 1
 	fi
 	
 	echo -e "Extracting $sampleid ...\n"
+	
+	# File extraction is not really neccessary in the case of our samples, as bioinformatic software in use is able to 
+	# work as well with fastq.gz as it does with its unpacked variant. Thus, it was only performed for reasearch and learning. 
+	
 	gunzip -fk $directoryurl/$sampleid 
 	
 	if [ "$4" == "filt" ] # Filter small nuclear sequences
@@ -33,14 +39,20 @@ then
 		echo -e "\nRemoving small nuclear sequences from contaminants database...\n"
 		sampleid=$(basename $downloadurl .gz)
 		mv $directoryurl/$sampleid $directoryurl/unfiltered_$sampleid
-		awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' $directoryurl/unfiltered_$sampleid | \
-		grep -vw 'small nuclear' $directoryurl/unfiltered_$sampleid > $directoryurl/$sampleid
 		
-		# other possibility would be to use seqkit grep funciton
-		# seqkit grep -vrnp '.*small nuclear.*' $directoryurl/unfiltered_$sampleid > $directoryurl/$sampleid	
+		seqkit grep -vrnp '.*small nuclear.*' $directoryurl/unfiltered_$sampleid > $directoryurl/$sampleid	
+		
+		# Another possibility could be to use an awk program in order to pre-process the fasta file, followed by reverse grep. 
+		# This may not be the best solution, as it could mess with the FASTA file structure, thus becoming illegible for
+	       	# other bioinformatic software.	
+		
+		# awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' $directoryurl/unfiltered_$sampleid | \
+                # grep -vw 'small nuclear' $directoryurl/unfiltered_$sampleid > $directoryurl/$sampleid
+
 	fi
 
 else
+	# Print a line to tell the user how to use the code.
 	echo -e "\nUsage: $0 <directoryurl> <downloadurl> <compression> <filter>\n"
 	exit 1
 fi
